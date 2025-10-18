2334241b340ebd07a3f4edc9b5b518817a293701
bagonjeremine17-source <bagonjeremine17@gmail.com>
2025-10-18 05:43:17 +0000

    Day 7

---
+++ Main.java
@@ -0,0 +1,61 @@
+
+// package class
+public class Main {
+
+        // reserve method
+        public static void main(String[] args) {
+
+
+            //method to ouput Hello World
+            System.out.println("Hello World");
+            Plant plant1=new Plant();
+            plant1.name="Papaya";
+            System.out.println(plant1.name);
+            System.out.println(plant1.getBranch());
+
+
+
+
+
+        }
+
+    }
+    //class
+    class Plant{
+        //variables
+        int Branch;
+        String name;
+        int leaves;
+
+        //method
+        void setBranch(int leaves){
+
+            this.leaves=leaves;
+
+
+
+        
+
+
+         } 
+         
+         public int getBranch() {
+             return Branch;
+         }
+
+
+    
+        }
+        
+
+
+
+
+
+
+
+
+    
+
+
+
