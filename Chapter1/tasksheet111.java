81e7f5b85d41410d0c95dc839a1203c9d4b2d7a3
bagonjeremine17-source <bagonjeremine17@gmail.com>
2025-11-28 23:55:50 +0000

    Task

---
+++ Chapter1/tasksheet111.java
@@ -0,0 +1,15 @@
+package Chapter1;
+
+public class tasksheet111 {
+
+    public static void main(String[] args) {
+        System.out.println("\nKamusta Mundo!");
+
+    
+
+
+
+    }
+
+  
+
--- Main.java
+++ Main.java
@@ -8,37 +8,16 @@ public class Main {
 
             //method to ouput Hello World
             System.out.println("Hello World");
-            Plant plant1=new Plant();
-            plant1.name="Papaya";
-            System.out.println(plant1.name);
-            System.out.println(plant1.getBranch());
+
+
+            
 
         }
 
     }
-    //class
-    class Plant{
-        //variables
-        int Branch;
-        String name;
-        int leaves;
-
-        //method
-        void setBranch(int leaves){
-
-            this.leaves=leaves;
+ 
 
 
-         } 
-         
-         public int getBranch() {
-             return Branch;
-         }
-
-
-    
-        }
-        
 
 
 
